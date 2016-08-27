2a4c5a1f052c8d80c4df6b5d7dee6e87a1fa23aa
Tanguy Gauvrit <tanguy@MBP-SDT.local>
2016-08-27 22:10:03 +0200

    Norm done

--- include/ft_malloc.h
+++ include/ft_malloc.h
@@ -6,7 +6,7 @@
 /*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/03 15:35:45 by tgauvrit          #+#    #+#             */
-/*   Updated: 2016/08/27 20:26:16 by tanguy           ###   ########.fr       */
+/*   Updated: 2016/08/27 22:09:23 by tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,25 @@
 # define MED_PAGE_MAX 32
 # define MED_MAX (32 * 4096)
 
-typedef struct s_link {
-	struct s_link *end;
-	struct s_link *next;
-} t_link;
+typedef struct		s_link {
+	struct s_link	*end;
+	struct s_link	*next;
+}					t_link;
 
-typedef struct s_data {
-	t_link *tny;
-	size_t tny_max;
-	t_link *med;
-	size_t med_max;
-	t_link *lrg;
-} t_data;
+typedef struct		s_data {
+	t_link			*tny;
+	size_t			tny_max;
+	t_link			*med;
+	size_t			med_max;
+	t_link			*lrg;
+}					t_data;
 
-extern t_data *saved;
+extern t_data		*g_saved;
 
-void	free(void *ptr);
-void	*malloc(size_t size);
-void	*realloc(void *ptr, size_t size);
-void	show_alloc_mem();
-t_data	*alloc_data();
+void				free(void *ptr);
+void				*malloc(size_t size);
+void				*realloc(void *ptr, size_t size);
+void				show_alloc_mem();
+t_data				*alloc_data();
 
 #endif
--- src/alloc_data.c
+++ src/alloc_data.c
@@ -6,33 +6,33 @@
 /*   By: tanguy <tanguy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/26 15:07:04 by tanguy            #+#    #+#             */
-/*   Updated: 2016/08/27 21:55:31 by tanguy           ###   ########.fr       */
+/*   Updated: 2016/08/27 22:06:48 by tanguy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
 
-t_data *saved = NULL;
+t_data *g_saved = NULL;
 
 t_data *alloc_data()
 {
-	if (saved == NULL)
+	if (g_saved == NULL)
 	{
-		saved = mmap(NULL, sizeof(t_data) + sizeof(t_link)
+		g_saved = mmap(NULL, sizeof(t_data) + sizeof(t_link)
 			+ getpagesize() * TNY_PAGE_MAX * 100
 			+ getpagesize() * MED_PAGE_MAX * 100,
 			PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
-		saved->tny_max = getpagesize() * TNY_PAGE_MAX * 100;
-		saved->tny = (t_link*)(saved + 1);
-		saved->tny->end = saved->tny + 1;
-		saved->tny->next = NULL;
-		saved->med_max = getpagesize() * MED_PAGE_MAX * 100;
-		saved->med = (t_link*)((char*)saved->tny + saved->tny_max);
-		saved->med->end = saved->med + 1;
-		saved->med->next = NULL;
-		saved->lrg = (t_link*)((char*)saved->med + saved->med_max);
-		saved->lrg->end = saved->lrg + 1;
-		saved->lrg->next = NULL;
+		g_saved->tny_max = getpagesize() * TNY_PAGE_MAX * 100;
+		g_saved->tny = (t_link*)(g_saved + 1);
+		g_saved->tny->end = g_saved->tny + 1;
+		g_saved->tny->next = NULL;
+		g_saved->med_max = getpagesize() * MED_PAGE_MAX * 100;
+		g_saved->med = (t_link*)((char*)g_saved->tny + g_saved->tny_max);
+		g_saved->med->end = g_saved->med + 1;
+		g_saved->med->next = NULL;
+		g_saved->lrg = (t_link*)((char*)g_saved->med + g_saved->med_max);
+		g_saved->lrg->end = g_saved->lrg + 1;
+		g_saved->lrg->next = NULL;
 	}
-	return (saved);
+	return (g_saved);
 }
